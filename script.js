aa0cb1af219a4cf99c17de9635ee06eaa231747e
tankistttt <levandemetrashvili10@gmail.com>
2024-02-11 20:03:55 +0400

    Adding course section

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const header = document.querySelector('header');
+const headerMask = document.querySelector('.header-mask');
 
 //*  Sticky navigation
 const headerHeight = header.getBoundingClientRect().height;
 const headerCallback = function (entries) {
   const [entry] = entries;
   console.log(entry);
+
   if (window.scrollY > 0) header.classList.add('active-header');
   else header.classList.remove('active-header');
 };
@@ -12,7 +14,6 @@ const headerCallback = function (entries) {
 const headerobserver = new IntersectionObserver(headerCallback, {
   root: null,
   threshold: 1,
-  rootMargin: `90px 0px -90px 0px`,
 });
 
-headerobserver.observe(header);
+headerobserver.observe(headerMask);
