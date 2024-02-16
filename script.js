2c5a60add9d89fe8a7ba9676398ecd5ba39a11b0
tankistttt <levandemetrashvili10@gmail.com>
2024-02-16 16:27:45 +0400

    Feat: add slider with dots, now wworking on auto slider

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+//* Variables
 const header = document.querySelector('header');
 const headerMask = document.querySelector('.header-mask');
+//
 const sliderEl = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const dotsContainer = document.querySelector('.dots');
+const dots = document.querySelectorAll('.dot');
 
 //*  Sticky navigation
-const headerHeight = header.getBoundingClientRect().height;
 const headerCallback = function (entries) {
   const [entry] = entries;
-  console.log(entry);
-
   if (window.scrollY > 0) header.classList.add('active-header');
   else header.classList.remove('active-header');
 };
@@ -21,3 +24,42 @@ const headerobserver = new IntersectionObserver(headerCallback, {
 headerobserver.observe(headerMask);
 
 //* Slider
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+function appearSlide(slide) {
+  slides.forEach((s, i) => {
+    if (i === slide) {
+      s.style.position = 'static';
+      dots[slide].style.backgroundColor = '#fff';
+    } else {
+      s.style.position = 'absolute';
+      dots[i].style.backgroundColor = '#e8e6e6';
+    }
+    s.classList.remove('slide--active');
+  });
+
+  setTimeout(() => {
+    slides[slide].classList.add('slide--active');
+  }, 200);
+}
+
+nextBtn.addEventListener('click', function () {
+  curSlide === maxSlide - 1 ? (curSlide = 0) : curSlide++;
+
+  appearSlide(curSlide);
+});
+
+prevBtn.addEventListener('click', function () {
+  curSlide === 0 ? (curSlide = maxSlide - 1) : curSlide--;
+
+  appearSlide(curSlide);
+});
+
+dotsContainer.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('dot')) return;
+  const slide = +e.target.dataset.slide;
+
+  appearSlide(slide);
+});
